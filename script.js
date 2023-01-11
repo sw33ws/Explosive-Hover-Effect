30575e89eeb689a83003ff38321dc663755663bb
sw33ws <gillisl22@outlook.com>
2023-01-11 12:41:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const enhance = id => {
+    const element = document.getElementById(id),
+        text = element.innerText.split("");
+
+    element.innerText = "";
+
+    text.forEach(letter => {
+        const span = document.createElement("span");
+
+        span.className = "letter";
+
+        span.innerText = letter;
+
+        element.appendChild(span);
+    })
+}
+
+enhance("channel-link")
